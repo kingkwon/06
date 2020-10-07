0062ed91555591eacb651a562e3e7f948a009116
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-10-07 23:19:56 +0900

    실습 04 : combination

--- main.c
+++ main.c
@@ -2,26 +2,45 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int a)
+int get_integer()
 {
-	return(a*a);
+	int x;
+	
+	printf("input a integer : ");
+	scanf("%d", &x);
+	return(x);
 }
-int main()
+int factorial(int x)
 {
-	int a=2;
-	a = square(a);
-	printf("a = %i\n", a);
+	int res=1;
+	int i;
+	
+	for (i=1; i<(x+1); i++)
+	{
+		res = res*i;
+	}
+	return res;
 }
-
-//void square(int a)
-//{
-//	a = a*a;
-//}
-
-//int main() {
+int Combination()
+{
+	int n, r;
+	int n_fac, nr_fac, r_fac;
 	
-//	int a = 2;
-//	square(a);
-//	printf("a = %i\n", a);
+	n = get_integer();
+	r = get_integer();
 	
-//}
+	n_fac = factorial(n);
+	nr_fac = factorial(n-r);
+	r_fac = factorial(r);
+	
+	return (n_fac/nr_fac/r_fac);
+}
+int main(void)
+{	
+	int res;
+	
+	res = Combination();
+	printf("The result is %d", res);
+	
+	return 0;
+}
